0163feea5f90379f5c339b0b422b8095d42aaddb
PrithaSaha48 <sahapritha819@gmail.com>
2021-07-27 14:25:07 +0600

    React js

---
+++ src/UsersData.jsx
@@ -0,0 +1,22 @@
+export const users =[ 
+    {
+        id: 1, 
+        name: 'pritha', 
+        dept: 'CSE'
+    },
+    {
+        id: 2, 
+        name: 'Saha', 
+        dept: 'CSSE'
+    },
+    {
+        id: 3, 
+        name: 'jui', 
+        dept: 'CIS'
+    },
+    {
+        id: 4, 
+        name: 'roy', 
+        dept: 'BBA'
+    },
+  ];
